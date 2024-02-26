05967977ca41e885cd5eb3bb54d6d275d7ae4e13
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2024-02-25 20:22:58 -0500

    added conversation history to each api call

--- api_calls/openai_api.py
+++ api_calls/openai_api.py
@@ -2,17 +2,23 @@ from openai import OpenAI
 
 # Initialize OpenAI
 openai = OpenAI()
-model = "gpt-3.5-turbo-16k"
+model = "gpt-4-0125-preview"
 
+
+history = ""
 
 def api_calls(user_message, sys_message):
+    global history
     messages = [
-                {"role": "system", "content":sys_message},
-                {"role": "user", "content":user_message}
-            ]
+        {"role": "system", "content": sys_message},
+        {"role": "user", "content": history+user_message}
+    ]
     response = openai.chat.completions.create(
         model=model,
         messages=messages,
         temperature=0.3
     )
+    history += response.choices[0].message.content
+    #print("history: ", history)
+
     return response.choices[0].message.content
