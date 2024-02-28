4623edddcc5db58ad3e55500ddd11a508c8841fb
drlordbasil <drlordbasil@gmail.com>
2024-02-28 09:22:54 -0500

    Add current code output text area

--- run.py
+++ run.py
@@ -56,12 +56,13 @@ class Application:
         self.current_ceo_message = ""
         self.current_image = ""
         self.current_career = ""
+        self.current_code_output = ""
 
         self.idea_text, self.idea_scrollbar = self.setup_labeled_text_area("Current Idea", 2, 0)
         self.code_text, self.code_scrollbar = self.setup_labeled_text_area("Current Code", 1, 1)
         self.feedback_text, self.feedback_scrollbar = self.setup_labeled_text_area("Current Feedback", 2, 2)
         self.ceo_message, self.ceo_scrollbar = self.setup_labeled_text_area("CEO Message", 2, 3)
-        self.code_text, self.code_scrollbar = self.setup_labeled_text_area("Current Code", 2, 1)
+        self.code_output_text, self.code_output_scrollbar = self.setup_labeled_text_area("Current Code Output", 2, 1)
         self.career_text, self.career_scrollbar = self.setup_labeled_text_area("Current Career", 1, 0)
         self.persona_text, self.persona_scrollbar = self.setup_labeled_text_area("Current Persona", 1, 2)
 
